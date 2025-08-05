f8a59449cd1744622ce894ff3ee344a82bb86c75
operations@inventzia.com <operations@inventzia.com>
2025-08-05 13:56:07 +0000

    Added box to cpt report 4

--- layouts.py
+++ layouts.py
@@ -270,12 +270,12 @@ def add_charts():
                 html.Button("Download PDF for PileID", id='download-pdf-btn', disabled=True),
                 dbc.Row([
                     dbc.Col(
-                            dcc.Graph(id="time_graph", style={"backgroundColor": "#193153",'width': '100%','marginBottom':'5px'}),#,'height': '400px'
+                            dcc.Graph(id="time_graph", style={"backgroundColor": "#193153",'width': '100%','marginBottom':'5px','height': '400px'}),#
                     xs=12, sm=12, md=12, lg=12, xl=12),
                     ]),
                 dbc.Row([
                     dbc.Col(
-                        dcc.Graph(id="depth_graph", style={"backgroundColor": "#193153",'marginTop':'5px'}),#,'height': '700px'
+                        dcc.Graph(id="depth_graph", style={"backgroundColor": "#193153",'marginTop':'5px','height': '700px'}),#
                         xs=12, sm=12, md=12, lg=12, xl=12),
                 ]),  # close Row config={'responsive': True},
 
